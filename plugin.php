11ea1a3f18c63261f69dd7f124612bd4c9171fa1
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-01 01:17:26 -0600

    Revise description that it's for developers

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Charm
  * Plugin URI: https://wpcharm.com/
- * Description: Framework that brings life to entities and relationships.
+ * Description: Charm is a WordPress framework for developers to build entities and relationships.
  * Version: 1.0.0
  * Requires at least: 5.3.2
  * Requires PHP: 7.3
