53bc11e3701a6ad44279f337038cbffee4f5006a
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-04 21:29:24 -0600

    Load all core and meta classes

--- autoloader.php
+++ autoloader.php
@@ -1,3 +1,14 @@
 <?php
 
-require_once WPMU_PLUGIN_DIR . '/charm/wordpress/Post.php';
+namespace Charm;
+
+if (!defined('ABSPATH')) {
+    die('Whatcha doin? Ƹ̵̡Ӝ̵̨̄Ʒ');
+}
+
+require_once WPMU_PLUGIN_DIR . '/charm/wordpress/core/Meta.php';
+require_once WPMU_PLUGIN_DIR . '/charm/wordpress/core/Post.php';
+require_once WPMU_PLUGIN_DIR . '/charm/wordpress/core/User.php';
+require_once WPMU_PLUGIN_DIR . '/charm/wordpress/meta/CommentMeta.php';
+require_once WPMU_PLUGIN_DIR . '/charm/wordpress/meta/PostMeta.php';
+require_once WPMU_PLUGIN_DIR . '/charm/wordpress/meta/UserMeta.php';
