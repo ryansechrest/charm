8661256bce28161f2c268da241876931cba170c5
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-08 23:46:32 -0500

    Load features and blueprints

--- autoloader.php
+++ autoloader.php
@@ -6,16 +6,16 @@ if (!defined('ABSPATH')) {
     die('Whatcha doin? Ƹ̵̡Ӝ̵̨̄Ʒ');
 }
 
+require_once WPMU_PLUGIN_DIR . '/charm/app/blueprint/Cast.php';
+require_once WPMU_PLUGIN_DIR . '/charm/app/blueprint/Entity.php';
 require_once WPMU_PLUGIN_DIR . '/charm/app/data-type/DateTime.php';
-require_once WPMU_PLUGIN_DIR . '/charm/app/feature/Conversion.php';
-require_once WPMU_PLUGIN_DIR . '/charm/app/feature/Crud.php';
+require_once WPMU_PLUGIN_DIR . '/charm/app/feature/Cast.php';
+require_once WPMU_PLUGIN_DIR . '/charm/app/feature/LoadProperties.php';
 require_once WPMU_PLUGIN_DIR . '/charm/app/core/Entity.php';
+require_once WPMU_PLUGIN_DIR . '/charm/app/Meta.php';
 require_once WPMU_PLUGIN_DIR . '/charm/app/Post.php';
 require_once WPMU_PLUGIN_DIR . '/charm/app/User.php';
 
 require_once WPMU_PLUGIN_DIR . '/charm/wordpress/Meta.php';
 require_once WPMU_PLUGIN_DIR . '/charm/wordpress/Post.php';
 require_once WPMU_PLUGIN_DIR . '/charm/wordpress/User.php';
-require_once WPMU_PLUGIN_DIR . '/charm/wordpress/meta/CommentMeta.php';
-require_once WPMU_PLUGIN_DIR . '/charm/wordpress/meta/PostMeta.php';
-require_once WPMU_PLUGIN_DIR . '/charm/wordpress/meta/UserMeta.php';
