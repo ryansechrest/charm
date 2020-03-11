95605dba9d94142739115c3c1c091d664c4e9476
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-10 22:50:45 -0500

    Add DateTime class

---
+++ data-type/DateTime.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Charm\App\DataType;
+
+use DateTime as DT;
+use DateTimeZone as DTZ;
+use Exception;
+
+/**
+ * Class DateTime
+ *
+ * @author Ryan Sechrest
+ * @package Charm\App\DataType
+ */
+class DateTime extends DT
+{
+    /**
+     * Initialize DateTime with timezone
+     *
+     * @param string $datetime
+     * @param string $timezone
+     * @return DateTime
+     */
+    public static function init(string $datetime, string $timezone = 'UTC'): DateTime
+    {
+        try {
+            $datetime = new DateTime($datetime, new DTZ($timezone));
+        } catch (Exception $e) {
+            // Quiet please.
+        }
+
+        return $datetime;
+    }
+
+    /************************************************************************************/
+    // Format methods
+
+    /**
+     * Format for WordPress database
+     *
+     * @return string
+     */
+    public function format_db(): string
+    {
+        return $this->format('Y-m-d H:i:s');
+    }
+}
