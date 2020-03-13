94979e82e2502640bc2d5f0a8b267a9b0d188a39
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-13 17:10:57 -0500

    Add Page class to preset post type

---
+++ app/Page.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Charm\App;
+
+/**
+ * Class Page
+ *
+ * @author Ryan Sechrest
+ * @package Charm\App
+ */
+class Page extends Post
+{
+    /**
+     * Post type
+     */
+    const POST_TYPE = 'page';
+}
