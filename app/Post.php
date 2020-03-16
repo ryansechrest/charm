3a4a60ef718095b9614391d47ed91ecb4b63f231
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-16 10:19:55 -0500

    Add ability to create meta on the fly

--- app/Post.php
+++ app/Post.php
@@ -16,7 +16,7 @@ class Post extends WpPost
     /**
      * Post type
      */
-    const POST_TYPE = '';
+    const POST_TYPE = 'post';
 
     /**
      * User class
@@ -114,7 +114,7 @@ class Post extends WpPost
      *
      * @return User|null
      */
-    public function post_author()
+    public function post_author(): User
     {
         if ($this->post_author_obj) {
             return $this->post_author_obj;
@@ -133,7 +133,7 @@ class Post extends WpPost
      *
      * @return Post|null
      */
-    public function post_parent()
+    public function post_parent(): Post
     {
         if ($this->post_parent_obj) {
             return $this->post_parent_obj;
@@ -147,26 +147,35 @@ class Post extends WpPost
         );
     }
 
+    /*----------------------------------------------------------------------------------*/
+
     /**
-     * Get metas
+     * Get or create meta(s)
      *
      * @param string $key
+     * @param mixed $value
      * @return Meta|Meta[]|null
      */
-    public function meta($key)
+    public function meta($key, $value = null)
     {
-        if (isset($this->metas[$key])) {
-            return $this->metas[$key];
+        if ($value !== null) {
+            $this->save_meta($key, $value);
         }
-        if (!$this->id) {
-            return null;
+
+        return $this->get_meta($key);
+    }
+
+    /**
+     * Get post meta from Post
+     *
+     * @param string $key
+     * @return Meta|Meta[]
+     */
+    private function get_meta(string $key)
+    {
+        if (count($this->metas) === 0) {
+            $this->metas = $this->get_metas();
         }
-        $this->metas = call_user_func(
-            static::META . '::init', [
-                'meta_type' => static::POST_TYPE,
-                'object_id' => $this->id,
-            ]
-        );
         if (!isset($this->metas[$key])) {
             return null;
         }
@@ -175,11 +184,87 @@ class Post extends WpPost
     }
 
     /**
+     * Get post metas from database
+     *
+     * @return array
+     */
+    private function get_metas(): array
+    {
+        return call_user_func(
+            static::META . '::init', [
+                'meta_type' => 'post',
+                'object_id' => $this->id,
+            ]
+        );
+    }
+
+    /**
+     * Create post meta instance
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return Meta
+     */
+    private function create_meta(string $key, $value)
+    {
+        $meta = static::META;
+
+        return new $meta([
+            'meta_type' => 'post',
+            'meta_key' => $key,
+            'meta_value' => $value,
+        ]);
+    }
+
+    /**
+     * Save post meta in Post
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    private function save_meta(string $key, $value): void
+    {
+        $meta = $this->create_meta($key, $value);
+        if (!isset($this->metas[$key])) {
+            $this->metas[$key] = $meta;
+            return;
+        }
+        if (is_array($this->metas[$key])) {
+            $this->metas[$key][] = $meta;
+            return;
+        }
+        $this->metas[$key] = [$this->metas[$key], $meta];
+    }
+
+    /**
+     * Save post metas in database
+     */
+    private function save_metas(): void
+    {
+        if (count($this->metas) === 0) {
+            return;
+        }
+        foreach ($this->metas as $key => $meta) {
+            if (!is_array($meta)) {
+                $meta->set_object_id($this->id);
+                $meta->save();
+                continue;
+            }
+            foreach ($meta as $single_meta) {
+                $single_meta->set_object_id($this->id);
+                $single_meta->save();
+            }
+        }
+    }
+
+    /*----------------------------------------------------------------------------------*/
+
+    /**
      * Get post date
      *
      * @return DateTime
      */
-    public function post_date()
+    public function post_date(): DateTime
     {
         $timezone = get_option('timezone_string');
 
@@ -193,7 +278,7 @@ class Post extends WpPost
      *
      * @return DateTime
      */
-    public function post_date_gmt()
+    public function post_date_gmt(): DateTime
     {
         return $this->post_date_gmt_obj = call_user_func(
             static::DATETIME, $this->post_date_gmt
@@ -205,7 +290,7 @@ class Post extends WpPost
      *
      * @return DateTime
      */
-    public function post_modified()
+    public function post_modified(): DateTime
     {
         $timezone = get_option('timezone_string');
 
@@ -219,10 +304,41 @@ class Post extends WpPost
      *
      * @return DateTime
      */
-    public function post_modified_gmt()
+    public function post_modified_gmt(): DateTime
     {
         return $this->post_modified_gmt_obj = call_user_func(
             static::DATETIME, $this->post_modified_gmt
         );
+    }
+
+    /************************************************************************************/
+    // Action methods
+
+    /**
+     * Create post with metas
+     *
+     * @return bool
+     */
+    public function create(): bool
+    {
+        if (!parent::create()) {
+            return false;
+        }
+        $this->save_metas();
+
+        return true;
+    }
+
+    /**
+     * Update post with metas
+     */
+    public function update(): bool
+    {
+        if (!parent::update()) {
+            return false;
+        }
+        $this->save_metas();
+
+        return true;
     }
 }
