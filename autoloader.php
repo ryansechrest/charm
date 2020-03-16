b9f70914a4d72936635d1382dc39a19c507b9f6a
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-16 12:55:58 -0500

    Add CommentMeta, PostMeta, and UserMeta class

--- autoloader.php
+++ autoloader.php
@@ -6,15 +6,27 @@ if (!defined('ABSPATH')) {
     die('Whatcha doin? Ƹ̵̡Ӝ̵̨̄Ʒ');
 }
 
+// WordPress Modules
 require_once WPMU_PLUGIN_DIR . '/charm/wordpress/module/PostType.php';
+
+// WordPress Classes
 require_once WPMU_PLUGIN_DIR . '/charm/wordpress/Meta.php';
 require_once WPMU_PLUGIN_DIR . '/charm/wordpress/Post.php';
 require_once WPMU_PLUGIN_DIR . '/charm/wordpress/User.php';
 
+// Data Types
 require_once WPMU_PLUGIN_DIR . '/charm/data-type/DateTime.php';
 
+// Modules
 require_once WPMU_PLUGIN_DIR . '/charm/module/PostType.php';
 
+// Meta Classes
 require_once WPMU_PLUGIN_DIR . '/charm/app/Meta.php';
+require_once WPMU_PLUGIN_DIR . '/charm/app/CommentMeta.php';
+require_once WPMU_PLUGIN_DIR . '/charm/app/PostMeta.php';
+require_once WPMU_PLUGIN_DIR . '/charm/app/UserMeta.php';
+
+// Entity Classes
 require_once WPMU_PLUGIN_DIR . '/charm/app/Post.php';
+require_once WPMU_PLUGIN_DIR . '/charm/app/Page.php';
 require_once WPMU_PLUGIN_DIR . '/charm/app/User.php';
