9182e2d4dbcbd7307cc8a1c45314d9cd0439d0df
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-16 12:42:46 -0500

    Add CommentMeta class

---
+++ app/CommentMeta.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Charm\App;
+
+/**
+ * Class CommentMeta
+ *
+ * @author Ryan Sechrest
+ * @package Charm\App
+ */
+class CommentMeta extends Meta
+{
+    /**
+     * Meta type
+     *
+     * @var string
+     */
+    const META_TYPE = 'comment';
+}
