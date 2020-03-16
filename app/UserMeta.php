937ea4b3edad55d692867d6cac2f044853867fec
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-16 12:43:11 -0500

    Add UserMeta class

---
+++ app/UserMeta.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Charm\App;
+
+/**
+ * Class UserMeta
+ *
+ * @author Ryan Sechrest
+ * @package Charm\App
+ */
+class UserMeta extends Meta
+{
+    /**
+     * Meta type
+     *
+     * @var string
+     */
+    const META_TYPE = 'user';
+}
