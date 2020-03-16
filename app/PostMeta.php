c22abead9c831d45e24d4cc4340eb57174681906
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-03-16 12:42:59 -0500

    Add PostMeta class

---
+++ app/PostMeta.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Charm\App;
+
+/**
+ * Class PostMeta
+ *
+ * @author Ryan Sechrest
+ * @package Charm\App
+ */
+class PostMeta extends Meta
+{
+    /**
+     * Meta type
+     *
+     * @var string
+     */
+    const META_TYPE = 'post';
+}
