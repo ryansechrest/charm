47ac84bab37ee00400e5cde7260eca3ad042679c
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-09-16 19:39:57 -0500

    Add name() and capabilities() method

--- skeleton/Taxonomy.php
+++ skeleton/Taxonomy.php
@@ -32,4 +32,39 @@ abstract class Taxonomy
      * @return TaxonomyModule
      */
     abstract public static function taxonomy(): TaxonomyModule;
+
+    /*----------------------------------------------------------------------------------*/
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public static function name(): string
+    {
+        return static::taxonomy()->get_name();
+    }
+
+    /**
+     * Get capabilities
+     *
+     * @param array $target_capabilities
+     * @return array
+     */
+    public static function capabilities($target_capabilities = []): array
+    {
+        $all_capabilities = static::taxonomy()->get_capabilities();
+        if (count($target_capabilities) === 0) {
+            return array_values($all_capabilities);
+        }
+        $mapped_capabilities = [];
+        foreach ($target_capabilities as $index => $target_capability) {
+            if (!isset($all_capabilities[$target_capability])) {
+                continue;
+            }
+            $mapped_capabilities[] = $all_capabilities[$target_capability];
+        }
+
+        return $mapped_capabilities;
+    }
 }
