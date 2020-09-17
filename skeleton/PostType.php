4b4d0f29d57fd4cbe6c28d88acfe2d5e0589befa
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-09-16 19:39:14 -0500

    Add name() and capabilities() method

--- skeleton/PostType.php
+++ skeleton/PostType.php
@@ -37,6 +37,39 @@ abstract class PostType
     /*----------------------------------------------------------------------------------*/
 
     /**
+     * Get name
+     *
+     * @return string
+     */
+    public static function name(): string
+    {
+        return static::post_type()->get_name();
+    }
+
+    /**
+     * Get capabilities
+     *
+     * @param array $target_capabilities
+     * @return array
+     */
+    public static function capabilities($target_capabilities = []): array
+    {
+        $all_capabilities = static::post_type()->get_capabilities();
+        if (count($target_capabilities) === 0) {
+            return array_values($all_capabilities);
+        }
+        $mapped_capabilities = [];
+        foreach ($target_capabilities as $index => $target_capability) {
+            if (!isset($all_capabilities[$target_capability])) {
+                continue;
+            }
+            $mapped_capabilities[] = $all_capabilities[$target_capability];
+        }
+
+        return $mapped_capabilities;
+    }
+
+    /**
      * Get taxonomies
      *
      * @return Taxonomy[]
