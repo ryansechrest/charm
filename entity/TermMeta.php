a4d8e4bffe0ad29a84cafbd13948e6c29ea3a649
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-09-16 19:37:01 -0500

    Add TermMeta class

---
+++ entity/TermMeta.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Charm\Entity;
+
+/**
+ * Class TermMeta
+ *
+ * @author Ryan Sechrest
+ * @package Charm\Entity
+ */
+class TermMeta extends Meta
+{
+    /************************************************************************************/
+    // Constants
+
+    /**
+     * Meta type
+     *
+     * @var string
+     */
+    const META_TYPE = 'term';
+}
