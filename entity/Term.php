205f3e256f5ff365401f14c723ab6d1eebf68a72
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-09-29 00:30:06 -0500

    Add parent and children methods

--- entity/Term.php
+++ entity/Term.php
@@ -24,4 +24,95 @@ class Term extends WpTerm
      * @var string
      */
     const META = 'Charm\Entity\TermMeta';
+
+    /************************************************************************************/
+    // Properties
+
+    /**
+     * Archive URL
+     *
+     * @var string
+     */
+    protected $archive_url = '';
+
+    /*----------------------------------------------------------------------------------*/
+
+    /**
+     * Parent object
+     *
+     * @var Term|null
+     */
+    protected $parent_obj = null;
+
+    /**
+     * Child objects
+     *
+     * @var Term[]
+     */
+    protected $child_objs = [];
+
+    /************************************************************************************/
+    // Object access methods
+
+    /**
+     * Get parent
+     *
+     * @return Term|null
+     */
+    public function parent(): ?Term
+    {
+        if ($this->parent_obj) {
+            return $this->parent_obj;
+        }
+        if (!$this->parent) {
+            return null;
+        }
+
+        return $this->parent_obj = static::init($this->parent);
+    }
+
+    /**
+     * Get children
+     *
+     * @return Term[]
+     */
+    public function children(): array
+    {
+        if ($this->child_objs) {
+            return $this->child_objs;
+        }
+        $children = get_ancestors($this->term_id, $this->taxonomy, 'taxonomy');
+        if (count($children) === 0) {
+            return [];
+        }
+
+        return $this->child_objs = array_map(function($term_id) {
+            return static::init($term_id);
+        }, $children);
+    }
+
+    /************************************************************************************/
+    // Get and set methods
+
+    /**
+     * Get archive URL
+     *
+     * @see get_term_link()
+     * @return string
+     */
+    public function get_archive_url(): string
+    {
+        if ($this->archive_url !== '') {
+            return $this->archive_url;
+        }
+        if ($this->term_id === 0) {
+            return '';
+        }
+        $archive_url= get_term_link($this->term_id, $this->taxonomy);
+        if (!is_string($archive_url)) {
+            return '';
+        }
+
+        return $this->archive_url = $archive_url;
+    }
 }
