07bd708a5da65bef7b45f6507d63c12f6e8ad49c
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-11-12 12:17:07 -0600

    Add MenuLocation module

---
+++ module/MenuLocation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Charm\Module;
+
+use Charm\WordPress\Module\MenuLocation as WpMenuLocation;
+
+/**
+ * Class MenuLocation
+ *
+ * @author Ryan Sechrest
+ * @package Charm\Module
+ */
+class MenuLocation extends WpMenuLocation
+{
+
+}
