1260f386092f44b6601d2410e7ba8b0f5f5f5730
Ryan Sechrest <ryan.sechrest@fleishman.com>
2020-12-07 13:23:52 -0600

    Add new_item and get_items method

--- entity/NavMenu.php
+++ entity/NavMenu.php
@@ -3,6 +3,8 @@
 namespace Charm\Entity;
 
 use Charm\WordPress\NavMenu as WpNavMenu;
+use ReflectionClass;
+use ReflectionException;
 
 /**
  * Class NavMenu
@@ -12,6 +14,16 @@ use Charm\WordPress\NavMenu as WpNavMenu;
  */
 class NavMenu extends WpNavMenu
 {
+    /************************************************************************************/
+    // Constants
+
+    /**
+     * NavMenuItem class
+     *
+     * @var string
+     */
+    const ITEM = 'Charm\Entity\NavMenuItem';
+    
     /************************************************************************************/
     // Object access methods
 
@@ -23,5 +35,41 @@ class NavMenu extends WpNavMenu
     public function id(): int
     {
         return $this->term_id;
+    }
+
+    /*----------------------------------------------------------------------------------*/
+
+    /**
+     * Create new nav menu item
+     *
+     * @param array $params
+     * @return bool
+     */
+    public function new_item($params = []): bool
+    {
+        $params['menu_id'] = $this->term_id;
+
+        try {
+            $reflection = new ReflectionClass(static::ITEM);
+            $nav_menu_item = $reflection->newInstanceArgs([$params]);
+            return $nav_menu_item->create();
+        } catch (ReflectionException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Get nav menu items
+     *
+     * @param array $params
+     * @return NavMenuItem[]
+     */
+    public function get_items($params = []): array
+    {
+        $params['menu_id'] = $this->term_id;
+
+        return call_user_func(
+            static::ITEM . '::get', $params
+        );
     }
 }
