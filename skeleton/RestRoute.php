3e676bcc5321de5081f8e66db6eadcab12c70e26
Ryan Sechrest <ryan.sechrest@fleishman.com>
2021-02-15 21:40:09 -0600

    Add RestRoute class

---
+++ skeleton/RestRoute.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Charm\Skeleton;
+
+use Charm\Module\Rest\Route as RestRouteModule;
+
+/**
+ * Class RestRoute
+ *
+ * @author Ryan Sechrest
+ * @package Charm\Skeleton
+ */
+abstract class RestRoute
+{
+    /************************************************************************************/
+    // Action methods
+
+    /**
+     * Register menu location
+     */
+    public static function register(): void
+    {
+        foreach (static::rest_routes() as $route) {
+            $route->register();
+        }
+    }
+
+    /************************************************************************************/
+    // Object access methods
+
+    /**
+     * Get REST route
+     *
+     * @return RestRouteModule[]
+     */
+    abstract public static function rest_routes(): array;
+}
