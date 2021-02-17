31d703ae211f8327599aab57036b038af3e71d30
Ryan Sechrest <ryan.sechrest@fleishman.com>
2021-02-17 13:45:22 -0600

    Add method to get theme path

--- helper/Path.php
+++ helper/Path.php
@@ -12,13 +12,25 @@ class Path
 {
     /**
      * URL to theme
-     *  e.g. https://domain.com/wp-content/theme/<name>/<path>
+     *  e.g. https://wpcharm.com/wp-content/themes/charm/<path>
      *
      * @param string $path
      * @return string
      */
-    public static function theme(string $path = ''): string
+    public static function theme_url(string $path = ''): string
     {
         return get_template_directory_uri() . '/' . $path;
+    }
+
+    /**
+     * Path to theme
+     *  e.g. /var/www/domains/wpcharm.com/wp-content/themes/charm/<path>
+     *
+     * @param string $path
+     * @return string
+     */
+    public static function theme_path(string $path = ''): string
+    {
+        return get_template_directory() . '/' . $path;
     }
 }
