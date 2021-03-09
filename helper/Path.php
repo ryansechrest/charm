f243557d2b04129f39a2923e22299c4eaba3f8ed
Ryan Sechrest <ryan.sechrest@fleishman.com>
2021-03-09 12:08:29 -0600

    Add site, login, and register URL methods

--- helper/Path.php
+++ helper/Path.php
@@ -11,6 +11,53 @@ namespace Charm\Helper;
 class Path
 {
     /**
+     * URL to login
+     *  e.g. https://wpcharm.com/wp-login.php?redirect_to=<path>
+     *
+     * @param string $path
+     * @return string
+     */
+    public static function login_url(string $path = ''): string
+    {
+        return wp_login_url($path);
+    }
+
+    /**
+     * URL to register
+     *  e.g. https://wpcharm.com/wp-login.php?action=register
+     *
+     * @return string
+     */
+    public static function register_url(): string
+    {
+        return wp_registration_url();
+    }
+
+    /**
+     * URL to site
+     *  e.g. https://wpcharm.com/<path>
+     *
+     * @param string $path
+     * @return string
+     */
+    public static function site_url(string $path = ''): string
+    {
+        return get_site_url() . '/' . $path;
+    }
+
+    /**
+     * Path to site
+     *  e.g. /var/www/domains/wpcharm.com/<path>
+     *
+     * @param string $path
+     * @return string
+     */
+    public static function site_path(string $path = ''): string
+    {
+        return get_home_path()  . $path;
+    }
+
+    /**
      * URL to theme
      *  e.g. https://wpcharm.com/wp-content/themes/charm/<path>
      *
