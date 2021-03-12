91cd439002af6d6c1b6fb1bac92c5b2e8fe3839b
Ryan Sechrest <ryan.sechrest@fleishman.com>
2021-03-12 10:23:08 -0600

    Add check methods

--- entity/Option.php
+++ entity/Option.php
@@ -25,4 +25,64 @@ class Option extends WpOption
     {
         return Cast::init($this->value);
     }
+
+    /************************************************************************************/
+    // Check methods
+
+    /**
+     * Has value changed?
+     *
+     * @return bool
+     */
+    public function changed(): bool
+    {
+        if ($this->value !== $this->prev_value) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Does value exist?
+     *
+     * @return bool
+     */
+    public function exists(): bool
+    {
+        return $this->value !== null;
+    }
+
+    /*----------------------------------------------------------------------------------*/
+
+    /**
+     * Is string blank?
+     *
+     * @return bool
+     */
+    public function is_blank(): bool
+    {
+        return $this->exists() && $this->value === '';
+    }
+
+    /**
+     * Is array empty?
+     *
+     *
+     * @return bool
+     */
+    public function is_empty(): bool
+    {
+        return $this->exists() && count($this->value) === 0;
+    }
+
+    /**
+     * Is integer zero?
+     *
+     * @return bool
+     */
+    public function is_zero(): bool
+    {
+        return $this->exists() && $this->value === 0;
+    }
 }
