f7b00566fe590067c9af36425b2a0907e4310d9d
Ryan Sechrest <ryan@ryansechrest.com>
2022-01-10 13:30:00 -0600

    Add current_url and current_url_with_args method

--- helper/Location.php
+++ helper/Location.php
@@ -10,10 +10,44 @@ namespace Charm\Helper;
  */
 class Location
 {
+
+    /**
+     * URL of current page without args
+     *  e.g. https://wpcharm.com/sample-page/
+     *
+     * @return string
+     */
+    public static function current_url(): string
+    {
+        $url = Location::current_url_with_args();
+        if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] !== '') {
+            $url = str_replace('?' . $_SERVER['QUERY_STRING'], '', $url);
+        }
+
+        return $url;
+    }
+
+    /**
+     * URL of current page with arguments
+     *  e.g. https://wpcharm.com/sample-page/?foo=bar
+     *
+     * @see get_site_url()
+     * @return string
+     */
+    public static function current_url_with_args(): string
+    {
+        if (!isset($_SERVER['REQUEST_URI'])) {
+            return '';
+        }
+
+        return get_site_url() . $_SERVER['REQUEST_URI'];
+    }
+
     /**
      * URL to WordPress user account login
      *  e.g. https://wpcharm.com/wp-login.php?redirect_to=<path>
      *
+     * @see wp_login_url()
      * @param string $path
      * @return string
      */
@@ -26,6 +60,7 @@ class Location
      * URL to WordPress user account registration
      *  e.g. https://wpcharm.com/wp-login.php?action=register
      *
+     * @see wp_registration_url()
      * @return string
      */
     public static function register_url(): string
@@ -37,6 +72,7 @@ class Location
      * URL to WordPress site
      *  e.g. https://wpcharm.com/<path>
      *
+     * @see get_site_url()
      * @param string $path
      * @return string
      */
@@ -49,6 +85,7 @@ class Location
      * Path to WordPress site
      *  e.g. /var/www/domains/wpcharm.com/<path>
      *
+     * @see get_home_path()
      * @param string $path
      * @return string
      */
@@ -61,6 +98,7 @@ class Location
      * URL to WordPress theme
      *  e.g. https://wpcharm.com/wp-content/themes/charm/<path>
      *
+     * @see get_template_directory_uri()
      * @param string $path
      * @return string
      */
@@ -73,11 +111,36 @@ class Location
      * Path to WordPress theme
      *  e.g. /var/www/domains/wpcharm.com/wp-content/themes/charm/<path>
      *
+     * @see get_template_directory()
      * @param string $path
      * @return string
      */
     public static function theme_path(string $path = ''): string
     {
         return get_template_directory() . '/' . $path;
+    }
+
+    /**
+     * URL to WordPress mu-plugins
+     *  e.g. https://wpcharm.com/wp-content/mu-plugins/<path>
+     *
+     * @param string $path
+     * @return string
+     */
+    public static function mu_plugin_url(string $path = ''): string
+    {
+        return WPMU_PLUGIN_URL . '/' . $path;
+    }
+
+    /**
+     * Path to WordPress mu-plugins
+     *  e.g. /var/www/domains/wpcharm.com/wp-content/mu-plugins/<path>
+     *
+     * @param string $path
+     * @return string
+     */
+    public static function mu_plugin_path(string $path = ''): string
+    {
+        return WPMU_PLUGIN_DIR . '/' . $path;
     }
 }
