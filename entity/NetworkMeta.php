793d812234122f24bfdce901da65154118880b88
Ryan Sechrest <ryan@ryansechrest.com>
2022-01-18 14:03:54 -0600

    Add class to interact with sitemeta table

---
+++ entity/NetworkMeta.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Charm\Entity;
+
+/**
+ * Class NetworkMeta
+ *
+ * @author Ryan Sechrest
+ * @package Charm\Entity
+ */
+class NetworkMeta extends Meta
+{
+    /************************************************************************************/
+    // Constants
+
+    /**
+     * Meta type
+     *
+     * @var string
+     */
+    const META_TYPE = 'site';
+}
