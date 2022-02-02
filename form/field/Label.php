e5111a2c872a171f60a196b6389c24095feb08b5
Ryan Sechrest <ryan@ryansechrest.com>
2022-02-02 14:04:11 -0600

    Add Label class

---
+++ form/field/Label.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Charm\Form\Field;
+
+/**
+ * Class Label
+ *
+ * @author Ryan Sechrest
+ * @package Charm\Form\Field
+ */
+class Label
+{
+    /************************************************************************************/
+    // Properties
+
+    /**
+     * For
+     *
+     * @var string
+     */
+    protected string $for = '';
+
+    /**
+     * Value
+     *
+     * @var string
+     */
+    protected string $value = '';
+
+    /************************************************************************************/
+    // Default constructor and load method
+
+    /**
+     * Label constructor
+     *
+     * @param array $data
+     */
+    public function __construct(array $data = [])
+    {
+        if (count($data) > 0) {
+            $this->load($data);
+        }
+    }
+
+    /**
+     * Load instance with data
+     *
+     * @param array $data
+     */
+    public function load(array $data): void
+    {
+        if (isset($data['for'])) {
+            $this->for = $data['for'];
+        }
+        if (isset($data['value'])) {
+            $this->value = $data['value'];
+        }
+    }
+
+    /************************************************************************************/
+    // Instantiation methods
+
+    /**
+     * New label
+     *
+     * @param array $data
+     * @return static
+     */
+    public static function init(array $data): Label
+    {
+        return new static($data);
+    }
+
+    /**
+     * Get new label as HTML
+     *
+     * @param array $data
+     * @return string
+     */
+    public static function html(array $data): string
+    {
+        return (static::init($data))->to_html();
+    }
+
+    /**
+     * Display new label as HTML
+     *
+     * @param array $data
+     */
+    public static function display(array $data): void
+    {
+        echo static::html($data);
+    }
+
+    /************************************************************************************/
+    // Cast methods
+
+    /**
+     * Cast instance to array
+     *
+     * @return array
+     */
+    public function to_array(): array
+    {
+        $data = [];
+        if ($this->for !== '') {
+            $data['for'] = $this->for;
+        }
+        if ($this->value !== '') {
+            $data['value'] = $this->value;
+        }
+
+        return $data;
+    }
+
+    /**
+     * Cast instance to HTML
+     *
+     * @return string
+     */
+    public function to_html(): string
+    {
+        return '<label for="' . $this->for . '">' . $this->value . '</label>';
+    }
+
+    /**
+     * Cast instance to JSON
+     *
+     * @return string
+     */
+    public function to_json(): string
+    {
+        return json_encode($this->to_array());
+    }
+    /**
+     * Cast instance to object
+     *
+     * @return object
+     */
+    public function to_object(): object
+    {
+        return (object) $this->to_array();
+    }
+
+    /************************************************************************************/
+    // Get and set methods
+
+    /**
+     * Get for
+     *
+     * @return string
+     */
+    public function get_for(): string
+    {
+        return $this->for;
+    }
+
+    /**
+     * Set for
+     *
+     * @param string $for
+     */
+    public function set_for(string $for): void
+    {
+        $this->for = $for;
+    }
+
+    /*----------------------------------------------------------------------------------*/
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function get_value(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     */
+    public function set_value(string $value): void
+    {
+        $this->value = $value;
+    }
+}
