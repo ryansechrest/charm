c1427964c8bfd74178b76b6c444dba6fc65bbf00
Ryan Sechrest <ryan@ryansechrest.com>
2022-02-03 16:01:42 -0600

    Add Radio class

---
+++ form/field/Radio.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Charm\Form\Field;
+
+/**
+ * Class Radio
+ *
+ * @author Ryan Sechrest
+ * @package Charm\Form\Field
+ */
+class Radio extends Checkbox
+{
+    /************************************************************************************/
+    // Constants
+
+    /**
+     * Type
+     *
+     * @var string
+     */
+    const TYPE = 'radio';
+}
