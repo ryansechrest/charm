1727c3a5b24119512d8c781dfe62e3d906e75914
Ryan Sechrest <ryan@ryansechrest.com>
2022-02-07 12:50:18 -0600

    Add class to extend WP version of class

---
+++ admin/MenuPage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Charm\Admin;
+
+use Charm\WordPress\Admin\MenuPage as WpMenuPage;
+
+/**
+ * Class MenuPage
+ *
+ * @author Ryan Sechrest
+ * @package Charm\Admin
+ */
+class MenuPage extends WpMenuPage
+{
+
+}
---
+++ admin/SubmenuPage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Charm\Admin;
+
+use Charm\WordPress\Admin\SubmenuPage as WpSubmenuPage;
+
+/**
+ * Class MenuPage
+ *
+ * @author Ryan Sechrest
+ * @package Charm\Admin
+ */
+class SubmenuPage extends WpSubmenuPage
+{
+
+}
