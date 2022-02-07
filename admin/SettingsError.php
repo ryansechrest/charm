828912850f2304f96accca5de42df042f3f2a2d5
Ryan Sechrest <ryan@ryansechrest.com>
2022-02-07 16:20:07 -0600

    Add SettingsError class

---
+++ admin/SettingsError.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Charm\Admin;
+
+use Charm\WordPress\Admin\SettingsError as WpSettingsError;
+
+/**
+ * Class SettingsError
+ *
+ * @author Ryan Sechrest
+ * @package Charm\Admin
+ */
+class SettingsError extends WpSettingsError
+{
+
+}
---
+++ wordpress/admin/SettingsError.php
@@ -0,0 +1,243 @@
+<?php
+
+namespace Charm\WordPress\Admin;
+
+/**
+ * Class SettingsError
+ *
+ * @author Ryan Sechrest
+ * @package Charm\WordPress\Admin
+ */
+class SettingsError
+{
+    /************************************************************************************/
+    // Properties
+
+    /**
+     * Title (Required)
+     *
+     * Slug title of the setting to which this error applies.
+     *
+     * @var string
+     */
+    protected string $title = '';
+
+    /**
+     * Slug (Required)
+     *
+     * Slug to identify the error. Used as part of 'id' attribute in HTML output.
+     *
+     * @var string
+     */
+    protected string $slug = '';
+
+    /**
+     * Message (Required)
+     *
+     * The formatted message text to display to the user (will be shown inside styled
+     * <div> and <p> tags).
+     *
+     * @var string
+     */
+    protected string $message = '';
+
+    /**
+     * Type
+     *
+     * Message type, controls HTML class. Possible values include 'error', 'success',
+     * 'warning', 'info'. Default value: 'error'
+     *
+     * @var string
+     */
+    protected string $type = 'error';
+
+    /************************************************************************************/
+    // Default constructor and load method
+
+    /**
+     * SettingsError constructor
+     *
+     * @param array $data
+     */
+    public function __construct(array $data = [])
+    {
+        if (count($data) === 0) {
+            return;
+        }
+        $this->load($data);
+    }
+
+    /**
+     * Load instance with data
+     *
+     * @param array $data
+     */
+    public function load(array $data): void
+    {
+        if (isset($data['title'])) {
+            $this->title = $data['title'];
+        }
+        if (isset($data['slug'])) {
+            $this->slug = $data['slug'];
+        }
+        if (isset($data['message'])) {
+            $this->message = $data['message'];
+        }
+        if (isset($data['type'])) {
+            $this->type = $data['type'];
+        }
+    }
+
+    /************************************************************************************/
+    // Action methods
+
+    /**
+     * Register settings error
+     */
+    public function register(): void
+    {
+        add_action('admin_init', function() {
+            add_settings_error(
+                $this->title,
+                $this->slug,
+                $this->message,
+                $this->type
+            );
+        });
+    }
+
+    /************************************************************************************/
+    // Cast methods
+
+    /**
+     * Cast properties to array
+     *
+     * @return array
+     */
+    public function to_array(): array
+    {
+        $data = [];
+        if ($this->title !== '') {
+            $data['title'] = $this->title;
+        }
+        if ($this->slug !== '') {
+            $data['slug'] = $this->slug;
+        }
+        if ($this->message !== '') {
+            $data['message'] = $this->message;
+        }
+        if ($this->type !== '') {
+            $data['type'] = $this->type;
+        }
+
+        return $data;
+    }
+
+    /**
+     * Cast instance to JSON
+     *
+     * @return string
+     */
+    public function to_json(): string
+    {
+        return json_encode($this->to_array());
+    }
+    /**
+     * Cast instance to object
+     *
+     * @return object
+     */
+    public function to_object(): object
+    {
+        return (object) $this->to_array();
+    }
+
+    /************************************************************************************/
+    // Get and set methods
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function get_title(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function set_title(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /*----------------------------------------------------------------------------------*/
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function get_slug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     */
+    public function set_slug(string $slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    /*----------------------------------------------------------------------------------*/
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function get_message(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     */
+    public function set_message(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    /*----------------------------------------------------------------------------------*/
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function get_type(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     */
+    public function set_type(string $type): void
+    {
+        $this->type = $type;
+    }
+}
