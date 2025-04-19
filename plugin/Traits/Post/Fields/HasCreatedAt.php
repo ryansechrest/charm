cdd80b2d71db87ddafd3d5f9e7ba0bc94cc313f4
Ryan Sechrest <ryan@sechrest.me>
2025-04-18 21:00:29 -0400

    Support string in `setCreatedAt()` as well as `DateTimeInterface`

--- plugin/Traits/Post/Fields/HasCreatedAt.php
+++ plugin/Traits/Post/Fields/HasCreatedAt.php
@@ -10,7 +10,7 @@ use DateTimeInterface;
  * Indicates that a post has a created date.
  *
  * Table: wp_posts
- * Column: post_created_gmt
+ * Column: post_date_gmt
  *
  * @author Ryan Sechrest
  * @package Charm
@@ -31,15 +31,17 @@ trait HasCreatedAt
     /**
      * Set created date
      *
-     * @param DateTimeInterface $dateTime
+     * @param DateTimeInterface|string $dateTime
      * @return static
      */
-    public function setCreatedAt(DateTimeInterface $dateTime): static
+    public function setCreatedAt(DateTimeInterface|string $dateTime): static
     {
+        $value = $dateTime instanceof DateTimeInterface
+            ? $dateTime->format('Y-m-d H:i:s')
+            : $dateTime;
+
         /** @var HasWpPost $this */
-        $this->wp()->setPostDateGmt(
-            $dateTime->format('Y-m-d H:i:s')
-        );
+        $this->wp()->setPostDateGmt($value);
 
         return $this;
     }
