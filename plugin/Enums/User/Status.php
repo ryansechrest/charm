d516f3a878f655a908d67bdeee9e1418abfa7efd
Ryan Sechrest <ryan@sechrest.me>
2025-04-18 21:05:09 -0400

    Add `User\Status` enum

---
+++ plugin/Enums/User/Status.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Charm\Enums\User;
+
+/**
+ * Indicates the status of a user.
+ *
+ * Table: wp_user
+ * Column: user_status
+ *
+ * @author Ryan Sechrest
+ * @package Charm
+ */
+enum Status: int
+{
+    // User is active
+    case ACTIVE = 0;
+}
