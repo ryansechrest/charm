4465c53414be486066183b60c8f6ad71733df68c
Ryan Sechrest <ryan@sechrest.me>
2025-04-18 21:07:58 -0400

    Add `Post` to represent a blog post in WordPress

---
+++ plugin/Models/Post.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Charm\Models;
+
+use Charm\Traits\Post\Fields\HasComments;
+use Charm\Traits\Post\Fields\HasContent;
+use Charm\Traits\Post\Fields\HasCreatedAt;
+use Charm\Traits\Post\Fields\HasCreator;
+use Charm\Traits\Post\Fields\HasExcerpt;
+use Charm\Traits\Post\Fields\HasId;
+use Charm\Traits\Post\Fields\HasPassword;
+use Charm\Traits\Post\Fields\HasPingbacks;
+use Charm\Traits\Post\Fields\HasSlug;
+use Charm\Traits\Post\Fields\HasStatus;
+use Charm\Traits\Post\Fields\HasTitle;
+use Charm\Traits\Post\Fields\HasUpdatedAt;
+use Charm\Traits\Post\HasPermalink;
+
+/**
+ * Represents a blog post in WordPress.
+ *
+ * @author Ryan Sechrest
+ * @package Charm
+ */
+class Post extends BasePost
+{
+    use HasId;
+    use HasCreator;
+    use HasCreatedAt;
+    use HasUpdatedAt;
+
+    use HasTitle;
+    use HasSlug;
+    use HasContent;
+    use HasExcerpt;
+
+    use HasStatus;
+    use HasPassword;
+    use HasPingbacks;
+    use HasComments;
+
+    use HasPermalink;
+
+    /**************************************************************************/
+
+    /**
+     * Post type
+     */
+    protected const POST_TYPE = 'post';
+}
