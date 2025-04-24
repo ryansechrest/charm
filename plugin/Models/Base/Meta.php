f8705105852bb17a3b9afbf66ab48bdfea8d1989
Ryan Sechrest <ryan@sechrest.me>
2025-04-23 22:06:28 -0400

    Add remaining methods, incl. `Cast` to transform values

--- plugin/Models/Base/Meta.php
+++ plugin/Models/Base/Meta.php
@@ -1,14 +1,19 @@
 <?php
 
-namespace Charm\Models;
+namespace Charm\Models\Base;
+
+use Charm\Models\WordPress;
+use Charm\Support\Cast;
+use Charm\Support\Result;
+use stdClass;
 
 /**
- * Represents a base meta field in WordPress.
+ * Represents a base meta in WordPress.
  *
  * @author Ryan Sechrest
  * @package Charm
  */
-abstract class BaseMeta
+abstract class Meta
 {
     /**
      * Meta type
@@ -56,5 +61,208 @@ abstract class BaseMeta
     public function wp(): ?WordPress\Meta
     {
         return $this->wpMeta;
+    }
+
+    /**************************************************************************/
+
+    /**
+     * Initialize meta
+     *
+     * @param int|stdClass $key
+     * @return ?static
+     */
+    public static function init(int|stdClass $key): ?static
+    {
+        if (!$wpMeta = WordPress\Meta::init(static::META_TYPE, $key)) {
+            return null;
+        }
+
+        $meta = new static();
+        $meta->wpMeta = $wpMeta;
+
+        return $meta;
+    }
+
+    /**
+     * Get first meta by specified key
+     *
+     * @param int $objectId
+     * @param string $metaKey
+     * @return ?static
+     */
+    public static function getFirst(int $objectId, string $metaKey): ?static
+    {
+        $metas = static::get($objectId, $metaKey);
+
+        if (!isset($metas[0])) {
+            return null;
+        }
+
+        return $metas[0];
+    }
+
+    /**
+     * Get all metas or by specified key
+     *
+     * @param int $objectId
+     * @param string $metaKey
+     * @return static[]
+     */
+    public static function get(int $objectId, string $metaKey = ''): array
+    {
+        $wpMetas = WordPress\Meta::get(
+            static::META_TYPE, $objectId, $metaKey
+        );
+
+        $metas = [];
+
+        foreach ($wpMetas as $wpMeta) {
+            $meta = new static();
+            $meta->wpMeta = $wpMeta;
+            $metas[] = $meta;
+        }
+
+        return $metas;
+    }
+
+    /**************************************************************************/
+
+    /**
+     * Save meta
+     *
+     * @return Result
+     */
+    public function save(): Result
+    {
+        return $this->wp()->save();
+    }
+
+    /**
+     * Create new meta
+     *
+     * @return Result
+     */
+    public function create(): Result
+    {
+        return $this->wp()->create();
+    }
+
+    /**
+     * Update existing meta
+     *
+     * @return Result
+     */
+    public function update(): Result
+    {
+        return $this->wp()->update();
+    }
+
+    /**
+     * Delete meta
+     *
+     * @return Result
+     */
+    public function delete(): Result
+    {
+        return $this->wp()->delete();
+    }
+
+    /**************************************************************************/
+
+    /**
+     * Get meta ID
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->wp()->getMetaId() ?? 0;
+    }
+
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * Get object ID
+     *
+     * @return int
+     */
+    public function getObjectId(): int
+    {
+        return $this->wp()->getObjectId() ?? 0;
+    }
+
+    /**
+     * Set object ID
+     *
+     * @param int $objectId
+     * @return static
+     */
+    public function setObjectId(int $objectId): static
+    {
+        $this->wp()->setObjectId($objectId);
+
+        return $this;
+    }
+
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * Get meta key
+     *
+     * @return string
+     */
+    public function getKey(): string
+    {
+        return $this->wp()->getMetaKey() ?? '';
+    }
+
+    /**
+     * Set meta key
+     *
+     * @param string $key
+     * @return static
+     */
+    public function setKey(string $key): static
+    {
+        $this->wp()->setMetaKey($key);
+
+        return $this;
+    }
+
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * Get meta value
+     *
+     * @return mixed
+     */
+    public function getValue(): mixed
+    {
+        return $this->wp()->getMetaValue() ?? null;
+    }
+
+    /**
+     * Set meta value
+     *
+     * @param mixed $value
+     * @return static
+     */
+    public function setValue(mixed $value): static
+    {
+        $this->wp()->setMetaValue($value);
+
+        return $this;
+    }
+
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * Pass value to Cast
+     *
+     * @return Cast
+     */
+    public function cast(): Cast
+    {
+        return Cast::from($this->wp()->getMetaValue());
     }
 }
