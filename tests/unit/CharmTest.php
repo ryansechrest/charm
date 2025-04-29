36fce9f3b9c67b24e76e4f12a34fefe858e54a94
Ryan Sechrest <ryan@sechrest.me>
2025-04-28 21:11:23 -0400

    Add test to test tests

---
+++ tests/unit/CharmTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CharmTest extends TestCase
+{
+    /**
+     * Let's write some tests, shall we?
+     */
+    public function testCharm()
+    {
+        $this->assertTrue(true);
+    }
+}
