eab02ac0b55b6ada962d8156e203eb5bbcf4b82d
Ryan Sechrest <ryan@sechrest.me>
2025-04-28 21:11:55 -0400

    Add test for `DateTime` utility

---
+++ tests/unit/Utilities/DateTimeTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Charm\Tests\Utilities;
+
+use Charm\Utilities\DateTime;
+use PHPUnit\Framework\TestCase;
+
+class DateTimeTest extends TestCase
+{
+    public function testInit()
+    {
+        $dateTime = DateTime::init('2023-01-10 03:42:00');
+
+        $this->assertInstanceOf(DateTime::class, $dateTime);
+    }
+
+    public function testTimezoneChange()
+    {
+        $timezone = 'America/Chicago';
+
+        $dateTime = DateTime::init('2023-01-10 03:42:00');
+        $actualTimezone = $dateTime->as($timezone)->format('e');
+
+        $this->assertSame($timezone, $actualTimezone);
+    }
+
+    public function testDisplayFormat()
+    {
+        $dateTime = DateTime::init('2023-01-10 03:42:00');
+        $actualFormat = $dateTime
+            ->as('America/Chicago')
+            ->format('F j, Y \a\t g:i a');
+
+        $this->assertSame('January 9, 2023 at 9:42 pm', $actualFormat);
+    }
+
+    public function testDatabaseFormat()
+    {
+        $wpGmtDateTime = '2023-01-10 03:42:00';
+        $dateTime = DateTime::init($wpGmtDateTime);
+
+        $this->assertSame($wpGmtDateTime, $dateTime->formatForDb());
+    }
+
+    public function testStringCast()
+    {
+        $wpGmtDateTime = '2023-01-10 03:42:00';
+        $dateTime = DateTime::init($wpGmtDateTime);
+
+        $this->assertSame(
+            'January 10, 2023 at 3:42 am', (string) $dateTime
+        );
+    }
+}
