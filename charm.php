0ff78942ab54b0b067b2a15bff4b1ec9915205e6
Ryan Sechrest <ryan@sechrest.me>
2025-04-29 20:30:24 -0400

    Add Charm plugin file

---
+++ charm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Charm;
+
+/**
+ * Plugin name: Charm <code>Ƹ̵̡Ӝ̵̨̄Ʒ</code>
+ * Plugin URI: https://github.com/ryansechrest/charm
+ * Version: 0.1.0
+ * Description: Charm is a modern framework for working with WordPress data through object-oriented models.
+ * Author: Ryan Sechrest
+ * Author URI: https://ryansechrest.com
+ * Text Domain: charm
+ * Tested up to: 6.8
+ * Requires at least: 6.8
+ * Requires PHP: 8.2
+ */
+
+if (!defined('ABSPATH')) {
+    exit('Charm <code>Ƹ̵̡Ӝ̵̨̄Ʒ</code>');
+}
+
+require_once WPMU_PLUGIN_DIR . '/charm/autoloader.php';
