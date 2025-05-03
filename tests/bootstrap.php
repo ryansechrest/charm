a7da6778905d2810338c738e973025f562b3f808
Ryan Sechrest <ryan@sechrest.me>
2025-05-02 23:52:19 -0400

    Bootstrap WordPress user functions for PHPUnit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,10 @@
 <?php
 
-// Load WordPress
+// Load WordPress core
 require_once dirname(__DIR__, 4) . '/wp-load.php';
+
+// Load WordPress user functions
+require_once ABSPATH . 'wp-admin/includes/user.php';
 
 // Load Charm
 require_once dirname(__DIR__) . '/autoloader.php';
