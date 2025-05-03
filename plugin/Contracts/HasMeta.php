11ec62fbb8600949ce8218ebe24e12400a660e8d
Ryan Sechrest <ryan@sechrest.me>
2025-05-03 00:04:19 -0400

    Update method names to reflect changes in `WithMeta` trait

--- plugin/Contracts/HasMeta.php
+++ plugin/Contracts/HasMeta.php
@@ -14,7 +14,14 @@ use Charm\Support\Result;
 interface HasMeta
 {
     /**
-     * Get single (or first) meta by key from cache or database
+     * Preload missing metas in cache from database
+     *
+     * @return static
+     */
+    function preloadMetas(): static;
+
+    /**
+     * Get single (or first) meta from cache or database
      *
      * @param string $key
      * @return ?Base\Meta
@@ -22,7 +29,7 @@ interface HasMeta
     function getMeta(string $key): ?Base\Meta;
 
     /**
-     * Get all metas by key from cache or database
+     * Get metas from cache or database
      *
      * @param string $key
      * @return Base\Meta[]
@@ -30,29 +37,48 @@ interface HasMeta
     function getMetas(string $key): array;
 
     /**
-     * Create or append meta to key in cache
+     * Create meta in cache
      *
      * @param string $key
      * @param mixed $value
      * @return Result
      */
-    function addMeta(string $key, mixed $value): Result;
+    function createMeta(string $key, mixed $value): Result;
 
     /**
-     * Overwrite meta(s) by key in cache
+     * Create or update meta in cache
      *
      * @param string $key
      * @param mixed $value
      * @return Result
      */
-    function setMeta(string $key, mixed $value): Result;
+    function updateMeta(string $key, mixed $value): Result;
 
     /**
-     * Delete all metas or specified value by key from cache
+     * Replace meta(s) in cache
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return Result
+     */
+    function replaceMeta(string $key, mixed $value): Result;
+
+    /**
+     * Delete all metas or specified value from cache
      *
      * @param string $key
      * @param mixed $value
      * @return Result
      */
     function deleteMeta(string $key, mixed $value = null): Result;
+
+    /**
+     * Persist metas in database and return results
+     *
+     * Called by base model in `create()` and `update()`.
+     *
+     * @param int $objectId
+     * @return Result[]
+     */
+    function persistMetas(int $objectId): array;
 }
