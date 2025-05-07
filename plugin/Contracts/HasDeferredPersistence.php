647d6dc00e7db4789fb76222bd660d8b3f6d7e91
Ryan Sechrest <ryan@sechrest.me>
2025-05-06 22:08:29 -0400

    Add `HasDeferredPersistence` contract for `WithRole` and `WithMeta`

---
+++ plugin/Contracts/HasDeferredPersistence.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Charm\Contracts;
+
+use Charm\Support\Result;
+
+/**
+ * Ensures that the model can defer persistence.
+ *
+ * @author Ryan Sechrest
+ * @package Charm
+ */
+interface HasDeferredPersistence
+{
+    /**
+     * Register a persistence method to be executed later
+     *
+     * @param string $method
+     * @return void
+     */
+    function registerPersistenceMethod(string $method): void;
+
+    /**
+     * Execute all registered persistence methods
+     *
+     * @return Result[]
+     */
+    function persistDeferred(): array;
+}
