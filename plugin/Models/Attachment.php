72f5cecb691b27d51539f21f394b7eb24472133f
Ryan Sechrest <ryan@sechrest.me>
2025-05-08 20:20:21 -0400

    Add `Attachment` model for WordPress files

---
+++ plugin/Models/Attachment.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Charm\Models;
+
+use Charm\Traits\Attachment\Fields\WithAttachedTo;
+use Charm\Traits\Attachment\Fields\WithCaption;
+use Charm\Traits\Attachment\Fields\WithDescription;
+use Charm\Traits\Attachment\Fields\WithMimeType;
+use Charm\Traits\Attachment\Metas\WithAltText;
+use Charm\Traits\Attachment\Metas\WithFilePath;
+use Charm\Traits\Attachment\Metas\WithMetaData;
+use Charm\Traits\Attachment\WithFileSize;
+use Charm\Traits\Post\Fields\WithCreatedAt;
+use Charm\Traits\Post\Fields\WithSlug;
+use Charm\Traits\Post\Fields\WithTitle;
+use Charm\Traits\Post\Fields\WithUpdatedAt;
+use Charm\Traits\Post\Fields\WithUser;
+use Charm\Traits\Post\WithPermalink;
+
+/**
+ * Represents an attachment in WordPress.
+ *
+ * @author Ryan Sechrest
+ * @package Charm
+ */
+class Attachment extends Base\Post
+{
+    // --- Attachment Fields ---------------------------------------------------
+
+    use WithUser;
+    use WithCreatedAt;
+    use WithUpdatedAt;
+
+    use WithTitle;
+    use WithSlug;
+    use WithCaption;
+    use WithDescription;
+
+    use WithAttachedTo;
+    use WithMimeType;
+
+    // --- Attachment Metas ----------------------------------------------------
+
+    use WithAltText;
+    use WithFilePath;
+    use WithMetaData;
+
+    // --- Attachment Helpers --------------------------------------------------
+
+    use WithFileSize;
+    use WithPermalink;
+
+    // *************************************************************************
+
+    /**
+     * Define post type
+     */
+    protected static function postType(): string
+    {
+        return 'attachment';
+    }
+}
