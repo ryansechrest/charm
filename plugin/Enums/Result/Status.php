7ddafd33c3898b4aec70abbfe778fa0f1c127933
Ryan Sechrest <ryan@sechrest.me>
2025-05-20 22:58:18 -0400

    Add `Status` enum to expand `Result` status to incl. `Warning`

---
+++ plugin/Enums/Result/Status.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Charm\Enums\Result;
+
+/**
+ * Indicates the status of a result.
+ *
+ * @author Ryan Sechrest
+ * @package Charm
+ */
+enum Status: string
+{
+    // The operation succeeded
+    case Success = 'success';
+
+    // The operation succeeded but triggered a warning
+    case Warning = 'warning';
+
+    // The operation failed
+    case Error = 'error';
+
+    // *************************************************************************
+
+    /**
+     * Get the status label.
+     *
+     * @return string
+     */
+    public function label(): string {
+        return match ($this) {
+            self::Success => __('Success', 'charm'),
+            self::Warning => __('Warning', 'charm'),
+            self::Error => __('Error', 'charm'),
+        };
+    }
+}
