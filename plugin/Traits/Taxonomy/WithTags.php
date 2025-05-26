1cbaa0399b9e36cf92d4a1bd84285dcbbeec2b89
Ryan Sechrest <ryan@sechrest.me>
2025-05-25 22:34:54 -0400

    Add `hasTag()` and `hasTags()`

--- plugin/Traits/Taxonomy/WithTags.php
+++ plugin/Traits/Taxonomy/WithTags.php
@@ -4,6 +4,7 @@ namespace Charm\Traits\Taxonomy;
 
 use Charm\Contracts\HasTerms;
 use Charm\Models\Terms\Tag;
+use Charm\Support\Result;
 
 /**
  * Adds tags to a model.
@@ -23,6 +24,25 @@ trait WithTags
         /** @var HasTerms $this */
         return $this->taxonomy(Tag::class)->getTerms();
     }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Add a tag to the model.
+     *
+     * $tag -> Category ID, slug, or `Tag` instance.
+     *
+     * @param int|string|Tag $tag foobar
+     * @return static
+     */
+    public function addTag(int|string|Tag $tag): static
+    {
+        /** @var HasTerms $this */
+        $this->taxonomy(Tag::class)->addTerm($tag);
+
+        return $this;
+    }
+
     /**
      * Add tags to the model.
      *
@@ -39,18 +59,20 @@ trait WithTags
         return $this;
     }
 
+    // -------------------------------------------------------------------------
+
     /**
-     * Add a tag to the model.
+     * Remove a tag from the model.
      *
-     * $tag -> Category ID, slug, or `Tag` instance.
+     * $tag -> Tag ID, slug, or `Tag` instance.
      *
      * @param int|string|Tag $tag foobar
      * @return static
      */
-    public function addTag(int|string|Tag $tag): static
+    public function removeTag(int|string|Tag $tag): static
     {
         /** @var HasTerms $this */
-        $this->taxonomy(Tag::class)->addTerm($tag);
+        $this->taxonomy(Tag::class)->removeTerm($tag);
 
         return $this;
     }
@@ -71,21 +93,7 @@ trait WithTags
         return $this;
     }
 
-    /**
-     * Remove a tag from the model.
-     *
-     * $tag -> Tag ID, slug, or `Tag` instance.
-     *
-     * @param int|string|Tag $tag foobar
-     * @return static
-     */
-    public function removeTag(int|string|Tag $tag): static
-    {
-        /** @var HasTerms $this */
-        $this->taxonomy(Tag::class)->removeTerm($tag);
-
-        return $this;
-    }
+    // -------------------------------------------------------------------------
 
     /**
      * Set tags on the model (replaces existing tags).
@@ -101,5 +109,31 @@ trait WithTags
         $this->taxonomy(Tag::class)->setTerms($tags);
 
         return $this;
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Check whether the model has the specified tag.
+     *
+     * @param int|string|Tag $tag foobar
+     * @return Result
+     */
+    public function hasTag(int|string|Tag $tag): Result
+    {
+        /** @var HasTerms $this */
+        return $this->taxonomy(Tag::class)->hasTerm($tag);
+    }
+
+    /**
+     * Check whether the model has at least one of the specified tags.
+     *
+     * @param array $tags [1, 'foobar', $tag]
+     * @return Result
+     */
+    public function hasTags(array $tags): Result
+    {
+        /** @var HasTerms $this */
+        return $this->taxonomy(Tag::class)->hasTerms($tags);
     }
 }
